54c1576150bc4c0121f008f82e21880ac49c1e52
JãoValim <110104557+m4loka@users.noreply.github.com>
2022-10-06 12:13:34 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,29 +1,30 @@
-let tabuada= 2;
-
+let tabuada = 5;
 
 function escreva(){
-
-    document.write(tabuada + " x 1 =" + (tabuada*1) + "<br>" );
-    document.write(tabuada + " x 2 =" + (tabuada*2) + "<br>" );
-    document.write(tabuada + " x 3 =" + (tabuada*3) + "<br>" );
-    document.write(tabuada + " x 4 =" + (tabuada*4) + "<br>" );
-    document.write(tabuada + " x 5 =" + (tabuada*5) + "<br>" );
-    document.write(tabuada + " x 6 =" + (tabuada*6) + "<br>" );
-    document.write(tabuada + " x 7 =" + (tabuada*7) + "<br>" );
-    document.write(tabuada + " x 8 =" + (tabuada*8) + "<br>" );
-    document.write(tabuada + " x 9 =" + (tabuada*9) + "<br>" );
-    document.write(tabuada + " x 10 =" + (tabuada*10) + "<br>" );
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
- 
-    function minhatabuada(){
-for(let i = 1; i <= 10; i++) {
-    for(let j = 1; j <= 10; j++){
-        document.write(i + " x " + j + " = " + (j*i) + "<br>");
+
+function minhaTabuada(){
+    for(let i = 10; i >= 1; i--){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x " + j + " = " + (j*i) + "<br>");
+        }
+        document.write("<br>");       
     }
-    document.write("<br>");
 }
-  }
-function cubo(){
-    (let)
 
+function cubo(){
+    for(let i = 20; i >= 2; i--){
+        document.write("O cubo de "+i+" é "+i+" x "+i+" x "+i+" = "+(i*i*i)+"<br>");
+    }
 }
