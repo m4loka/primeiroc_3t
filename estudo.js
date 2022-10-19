5af67130a9ff1f7d013a5ffa6eaa00a3631fd1c5
JãoValim <110104557+m4loka@users.noreply.github.com>
2022-10-19 10:00:50 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -41,3 +41,19 @@ function mat(){
     document.write("Resultado" + r);
 
 }
+function media(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
+    let r = (Number(n1) + (Number(n2) + (Number(n3)) /3;
+
+document.getElementById("resultado").innerHTML = "média: " + r;
+}
+function soma(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
+    let r = (Number(n1) + (Number(n2) + (Number(n3);
+
+document.getElementById("resultado").innerHTML = "média: " + r;
+}
