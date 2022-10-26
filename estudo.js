a421894cd8f3d21837138c2434661246ffd7ca5f
JãoValim <110104557+m4loka@users.noreply.github.com>
2022-10-26 09:29:16 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -28,32 +28,37 @@ function cubo(){
         document.write("O cubo de "+i+" é "+i+" x "+i+" x "+i+" = "+(i*i*i)+"<br>");
     }
 }
-
-function mat(){
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+function mat(){  
     let v = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
-    let t = document.getElementById("meses").value;
+    let t = document.getElementById("messes").value;
+  
+    if(!Number(v)){
+        alert("O Capitual deve ser numérico.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+    if(!Number(j)){
+        alert("Juros deve ser número.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
+    }
+    if(!Number(t)){
+        alert("Juros deve ser número.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
     let r = 0;
     for(let i=1; i<=t; i++){
         r = v * (1+(j/100));
         v = r;
+        document.write("Mês " + i + " = " + moeda(r) + "<br>");
     }
-    document.write("Resultado" + r);
-
-}
-function media(){
-    let n1 = document.getElementById("v1").value;
-    let n2 = document.getElementById("v2").value;
-    let n3 = document.getElementById("v3").value;
-    let r = (Number(n1) + (Number(n2) + (Number(n3)) /3;
-
-document.getElementById("resultado").innerHTML = "média: " + r;
-}
-function soma(){
-    let n1 = document.getElementById("v1").value;
-    let n2 = document.getElementById("v2").value;
-    let n3 = document.getElementById("v3").value;
-    let r = (Number(n1) + (Number(n2) + (Number(n3);
-
-document.getElementById("resultado").innerHTML = "média: " + r;
+    document.write("Resultado " + moeda(r));
 }
